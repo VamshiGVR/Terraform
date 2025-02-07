fdb55e3b6d07a56c541832acead365b75e1898c4
Vamshimittu1 <gunvamshidharreddy@gmail.com>
2025-02-07 15:45:51 -0500

    added senstive file

--- COURSE/01_basics/main.tf
+++ COURSE/01_basics/main.tf
@@ -1,7 +1,14 @@
 # simple file resource
 # Resource block
+# normal file
 resource "local_file" "local_name" {
   filename = "C:/Users/ATGWORKS/OneDrive/Desktop/Terraform/COURSE/01_basics/example.txt"
   #filename ="${path.module}/example.txt"
   content = "This line is updated"
+}
+
+# Sentivegile
+resource "local_sensitive_file" "local_sensitive_name" {
+  filename = "C:/Users/ATGWORKS/OneDrive/Desktop/Terraform/COURSE/01_basics/example_senstive.txt"
+  content = "This is a senstive information"
 }
