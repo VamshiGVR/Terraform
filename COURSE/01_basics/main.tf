77cec04cdec479e11c34479aa54018f85737b300
Vamshimittu1 <gunvamshidharreddy@gmail.com>
2025-02-07 16:05:59 -0500

    terraform updated

--- COURSE/01_basics/main.tf
+++ COURSE/01_basics/main.tf
@@ -1,9 +1,8 @@
 # simple file resource
 # Resource block
 # normal file
-
 ## Type = same for multiple resources but reference name should be different.
-## Type = differnt then reference name can be same.
+## Type = diffrent then reference name can be same.
 resource "local_file" "local_name" {
   filename = "C:/Users/ATGWORKS/OneDrive/Desktop/Terraform/COURSE/01_basics/localFile.txt"
   #filename ="${path.module}/localFile.txt"
