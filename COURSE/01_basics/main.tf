b512c0082d8974244a1e698eeba57c10111c5747
Vamshimittu1 <gunvamshidharreddy@gmail.com>
2025-02-07 15:52:39 -0500

    add files

--- COURSE/01_basics/main.tf
+++ COURSE/01_basics/main.tf
@@ -1,14 +1,17 @@
 # simple file resource
 # Resource block
 # normal file
+
+## Type = same for multiple resources but reference name should be different.
+## Type = differnt then reference name can be same.
 resource "local_file" "local_name" {
   filename = "C:/Users/ATGWORKS/OneDrive/Desktop/Terraform/COURSE/01_basics/example.txt"
-  #filename ="${path.module}/example.txt"
+  #filename ="${path.module}/localFile.txt"
   content = "This line is updated"
 }
 
-# Sentivegile
+# Sentive File
 resource "local_sensitive_file" "local_sensitive_name" {
-  filename = "C:/Users/ATGWORKS/OneDrive/Desktop/Terraform/COURSE/01_basics/example_senstive.txt"
-  content = "This is a senstive information"
+  filename = "C:/Users/ATGWORKS/OneDrive/Desktop/Terraform/COURSE/01_basics/senstiveFile.txt"
+  content  = "This is a senstive information."
 }
