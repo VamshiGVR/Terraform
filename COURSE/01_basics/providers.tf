8953f27e5c9942ac318fb71eea59e1fe9504e1b9
Vamshimittu1 <gunvamshidharreddy@gmail.com>
2025-02-07 15:14:07 -0500

    Terraform Init

---
+++ COURSE/01_basics/providers.tf
@@ -0,0 +1,12 @@
+# terraform block
+terraform {
+  required_providers {
+    # providers
+    local = {
+        source = "hashicorp/local"
+        version = "2.5.1"
+    }
+  }
+}
+
+#provider block
