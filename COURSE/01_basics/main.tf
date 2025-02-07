c8403e4ed9b1deaf64af57a5f63a3dea71ef6554
Vamshimittu1 <gunvamshidharreddy@gmail.com>
2025-02-07 15:59:06 -0500

    terraform plan -out=plan

--- COURSE/01_basics/main.tf
+++ COURSE/01_basics/main.tf
@@ -5,13 +5,13 @@
 ## Type = same for multiple resources but reference name should be different.
 ## Type = differnt then reference name can be same.
 resource "local_file" "local_name" {
-  filename = "C:/Users/ATGWORKS/OneDrive/Desktop/Terraform/COURSE/01_basics/example.txt"
+  filename = "C:/Users/ATGWORKS/OneDrive/Desktop/Terraform/COURSE/01_basics/localFile.txt"
   #filename ="${path.module}/localFile.txt"
   content = "This line is updated"
 }
 
 # Sentive File
 resource "local_sensitive_file" "local_sensitive_name" {
-  filename = "C:/Users/ATGWORKS/OneDrive/Desktop/Terraform/COURSE/01_basics/senstiveFile.txt"
+  filename = "C:/Users/ATGWORKS/OneDrive/Desktop/Terraform/COURSE/01_basics/localSenstiveFile.txt"
   content  = "This is a senstive information."
 }
