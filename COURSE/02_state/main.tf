37c25c384debad787e6bbef359c38a3183d2e57f
Vamshimittu1 <gunvamshidharreddy@gmail.com>
2025-02-10 15:54:37 -0500

    added

--- COURSE/02_state/main.tf
+++ COURSE/02_state/main.tf
@@ -1,14 +1,14 @@
-resource "local_file" "filename1" {
+resource "local_file" "FileName1" {
   content = "This is for state - 1"
   filename= "${path.module}/filename1.txt"
 }
 
-resource "local_file" "filename2" {
+resource "local_file" "FileName2" {
   content = "This is for state - 2"
   filename= "${path.module}/filename2.txt"
 }
 
-resource "local_sensitive_file" "sensitivefilename" {
+resource "local_sensitive_file" "sensitiveFileName" {
   content = "This is for secert"
   filename= "${path.module}/sensitivefilename.txt"
 }
