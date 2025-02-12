1667ddc905417e35e35144d461a2c76da6b8b1a5
Vamshimittu1 <gunvamshidharreddy@gmail.com>
2025-02-11 19:08:02 -0500

    added

--- COURSE/01_basics/main.tf
+++ COURSE/01_basics/main.tf
@@ -9,7 +9,7 @@ resource "local_file" "local_name" {
   content = "This line is updated"
 }
 
-# Sentive File
+# Sentive File.
 resource "local_sensitive_file" "local_sensitive_name" {
   filename = "C:/Users/ATGWORKS/OneDrive/Desktop/Terraform/COURSE/01_basics/localSenstiveFile.txt"
   content  = "This is a senstive information."
